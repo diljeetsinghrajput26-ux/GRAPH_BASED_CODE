52fea95a5ef339a251bc0a263337e22ab8f4ef9b
diljeetsinghrajput26@gmail.com <diljeetsinghrajput26@gmail.com>
2026-03-03 12:14:37 +0530

    changing the code

--- BREATH_FIRST_search.cpp
+++ BREATH_FIRST_search.cpp
@@ -15,11 +15,11 @@ void addEdge(int u,int v){
     l[u].push_back(v);
     l[v].push_back(u);
 }
-void bfs(){
+void bfs(int start){
 queue<int>Q;
 vector<bool>vis(v,false);
-Q.push(0);
-vis[0]=true;
+Q.push(start);
+vis[start]=true;
 while(Q.size()>0){
     int u=Q.front();
     Q.pop();
@@ -37,12 +37,11 @@ while(Q.size()>0){
 };
 int main(){
     graph g(5);
-    g.addEdge(0,1);
     g.addEdge(1,2);
-    g.addEdge(1,3);
+    g.addEdge(1,4);
     g.addEdge(2,3);
-    g.addEdge(2,4);
-    g.bfs();
+    g.addEdge(3,4);
+    g.bfs(1);
     return 0;
 
 }
